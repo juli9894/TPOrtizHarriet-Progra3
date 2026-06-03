12bc65f6f8eeb2b4e92ab8ba344bffc77f6cb893
juli9894 <eze9894@gmail.com>
2026-06-03 20:47:15 -0300

    Creacion de vistas, productos y carrito

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,14 @@ const mainController = {
 
     home: (req, res) => {
         res.render('index');
+    },
+
+    productos: (req, res) => {
+        res.render('productos');
+    },
+
+    carrito: (req, res) => {
+        res.render('carrito');
     }
 };
 
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.home);
+router.get('/productos', mainController.productos);
+router.get('/carrito', mainController.carrito);
 
 module.exports = router;
